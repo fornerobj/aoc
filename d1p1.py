346fb088776f678635110b4f66823a7fb6e156e7
fornerobj <fornerobj@gmail.com>
2023-12-12 12:12:14 -0600

    Day 1 part 2 passing

--- d1p1.py
+++ d1p1.py
@@ -1,12 +1,19 @@
 file1 = open('input', 'r')
 lines = file1.readlines()
 
-sum = 0
-digits = []
-for line in lines:
-    for c in line:
-        if c.isdigit():
-            digits.append(c)
-    sum = sum + int(digits[0]+digits[-1])
+def fun(line):
     digits = []
+    for i in range (len(line)):
+        if(line[i].isdigit()):
+            digits.append(line[i])
+        for j, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+            if(line[i::].startswith(val)):
+                digits.append(str(j+1))
+    return digits
+
+sum = 0
+for line in lines:
+    digits = fun(line)
+    sum = sum + int(digits[0] + digits[-1])
+
 print(sum)
