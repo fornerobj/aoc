7c34dfe01c17f06abc78ebf420774cda27d7d30e
Brandon Fornero <fornerobj@gmail.com>
2023-12-14 00:03:04 -0600

    day 4 p1 passing

---
+++ day4/d4p1.py
@@ -0,0 +1,22 @@
+with open('input', 'r') as file1:
+    lines = file1.read().splitlines()
+
+def noEmpty(s):
+    return s != ''
+
+total = 0
+for i, line in enumerate(lines):
+    points = 0
+    line = line.split(':')[1].strip()
+    winners = list(filter(noEmpty, line.split('|')[0].strip().split(' ')))
+    scratches = list(filter(noEmpty, line.split('|')[1].strip().split(' ')))
+    nums = []
+    for s in scratches: 
+        if s in winners:
+            nums.append(s)
+            if points == 0:
+                points += 1
+            else:
+                points *= 2
+    total += points
+print(total)
